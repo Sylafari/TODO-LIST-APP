1a70b3a8a4430fd5377e40b6c0e6cd84e325bbd9
Quinn Cheng <sylafari@gmail.com>
2020-09-09 22:42:29 -0700

    Contact Contains State Now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+/*
 const Contact = () => {
     return (
         <div className="container">
@@ -15,5 +16,54 @@ const Contact = () => {
         </div>
     )
 }
+
+*/
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          firstName: "",
+          lastName: "",
+          eMail: "",
+          cComments: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+      }
+      handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+      }
+      render() {
+        return (
+          <form onSubmit>
+          <p className = "center">Enter your first name:</p>
+          <input
+            type='text'
+            name='firstName'
+            onChange={this.handleChange}
+          />
+          <p className = "center">Enter your last name:</p>
+          <input
+            type='text'
+            name='lastName'
+            onChange={this.handleChange}
+          />
+          <p className = "center">Enter your email:</p>
+          <input
+            type='text'
+            name='eMail'
+            onChange={this.handleChange}
+          />
+          <p className = "center">Enter your Comments:</p>
+          <input
+            type='text'
+            name='cComments'
+            onChange={this.handleChange}
+          />
+          <input type = 'submit' />
+          </form>
+        );
+      }
+    }
 
 export default Contact
